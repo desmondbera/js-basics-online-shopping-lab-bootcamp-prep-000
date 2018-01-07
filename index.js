503bbfb4747fb286da98d525cbf90fe59e0ccf2f
desmond beramendi <desmond7187@gmail.com>
2018-01-07 06:17:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,8 +70,5 @@ function placeOrder(cardNumber) {
   } else if(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
-  
-  
-  
-
+ cart = []
 }
