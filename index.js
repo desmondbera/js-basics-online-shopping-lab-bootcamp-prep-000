9102ed4861f9829c41a164ad6ebd232b2085aed1
desmond beramendi <desmond7187@gmail.com>
2018-01-07 06:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     console.log(`Sorry, we don't have a credit card on file for you.`)
   } else if(cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to your ${cardNumber}.`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
   
   
