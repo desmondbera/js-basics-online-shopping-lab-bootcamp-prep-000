96b802abd774cf5a76b9c5fe9301f4b0b181a350
desmond beramendi <desmond7187@gmail.com>
2018-01-07 05:58:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,17 +51,14 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var hasItemInCart = false;
   for(var i = 0; i < cart.length; i++) {
     if(cart[i].hasOwnProperty(item)) {
-      cart.splice(i, 0)
+      cart.splice(i, 1)
+    } else {
+      console.log("That item is not in your cart.")
     }
   }
-  
-  
-  if(!hasItemInCart) {
-    console.log("That item is not in your cart.")
-  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
