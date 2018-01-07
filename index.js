a52b6ffcb955c1f6a50ddc760a1d35ecc055c4b5
desmond beramendi <desmond7187@gmail.com>
2018-01-07 06:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,15 +65,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
-  
-  if(cardNumber) {
+  if(!isCardNumPresent) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else if(cardNumber) {
     console.log(`Your total cost is ${total()}, which will be charged to your ${cardNumber}`)
   }
   
   
   
-  if(!isCardNumPresent) {
-    console.log("Sorry, we don't have a credit card on file for you.")
-  }
+
 }
